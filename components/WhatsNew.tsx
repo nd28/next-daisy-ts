88da1f63adaf2eb5b62bc5008aa173318198040a
Nilesh <sutharnilesh1901@gmail.com>
2023-03-05 10:58:35 +0000

    Update & Fix Mistake in WhatsNew.tsx

--- components/WhatsNew.tsx
+++ components/WhatsNew.tsx
@@ -1,14 +1,15 @@
-const WhatsNew: React.FC<{ open?:boolean }> = ({open = false}) => {
-// Card
-const whatsNewCard = <div className="card w-96 bg-primary text-primary-content">
-  <div className="card-body">
-    <h2 className="card-title">Card title!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn">Buy Now</button>
+const WhatsNew: React.FC<{ open?: boolean }> = ({ open = false }) => {
+  // Card
+  const whatsNewCard = (
+    <div className="card w-96 bg-primary text-primary-content">
+      <div className="card-body">
+        <h2 className="card-title">Card title!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-end">
+          <button className="btn">Buy Now</button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-
-}
-export const 
+  );
+};
+export const WhatsNew;
